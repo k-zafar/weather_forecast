20d087778570f2627fd5e8786901b63fd6b7ee61
Kamran Zafar <kamran.zafar@cognitiveaxis.com>
2019-11-27 16:24:04 +0500

    Adding exception when city is invalid.

--- src/ShowWeatherForeacast.php
+++ src/ShowWeatherForeacast.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\weather_forecast;
+use GuzzleHttp\Exception\RequestException;
 
 /**
  * Class ShowWeatherForeacast.
@@ -24,24 +25,29 @@ class ShowWeatherForeacast {
 
     $client = \Drupal::httpClient();
     
-    // Calling Weather API
-    $request = $client->get('http://api.openweathermap.org/data/2.5/weather', [
-    	'query' => [
-    		'q' => $city,
-    		'APPID' => 'd5b252a4a2dcb9addc5b1d38a962fc0f',
-    		'units' => 'imperial',
-    	],
-	]);
+    try {
+        // Calling Weather API
+        $request = $client->get('http://api.openweathermap.org/data/2.5/weather', [
+                'query' => [
+                'q' => $city,
+                'APPID' => 'd5b252a4a2dcb9addc5b1d38a962fc0f',
+                'units' => 'imperial',
+            ],
+        ]);
 
-	// Extracting weather forecast
-	$response = json_decode($request->getBody(), true);
-	$weather_forecast['wind']  = $response['wind']['speed'];
-	$weather_forecast['humidity'] = $response['main']['humidity'];
-	$weather_forecast['pressure'] = $response['main']['pressure'];
-	$weather_forecast['temp'] = $response['main']['temp'];
-	$weather_forecast['weather'] = $response['weather'][0]['main'];
-	$weather_forecast['city'] = $city;
-
+        // Extracting weather forecast
+        $response = json_decode($request->getBody(), true);
+        $weather_forecast['wind']  = $response['wind']['speed'];
+        $weather_forecast['humidity'] = $response['main']['humidity'];
+        $weather_forecast['pressure'] = $response['main']['pressure'];
+        $weather_forecast['temp'] = $response['main']['temp'];
+        $weather_forecast['weather'] = $response['weather'][0]['main'];
+        $weather_forecast['city'] = $city;
+    }
+    catch (RequestException $e) {
+        // Add Drupal Log
+    }
+	
     return $weather_forecast;
   }
 }
